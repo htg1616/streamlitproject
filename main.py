0f93564ecd5ace2c5629d52971d88714f6a6f7c5
htg1616 <htg1616@gmail.com>
2024-06-08 23:43:42 +0900

    start_end 입력 부분 column 추가 graph one_date 수정 메인 페이지 작성 카카오 삭제 및 주석 작성 완료

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import yfinance as yf
 from ing_theme_matplotlib import mpl_style
 from PIL import Image
 
-#matplotlib 스타일 설정
+# matplotlib 스타일 설정
 mpl_style(dark=True)
 
 # 한글 폰트 설정
@@ -13,6 +13,10 @@ plt.rcParams['font.family'] = 'Malgun Gothic'
 plt.rcParams['axes.unicode_minus'] = False
 
 def page_home():
+    """
+    홈 페이지를 렌더링함.
+    이미지와 텍스트를 포함함.
+    """
     st.title('ETF 투자')
     st.subheader('내가 대구과학고에서 성공할 수 있었던 이유')
     img = Image.open('image.jpg')
@@ -32,16 +36,14 @@ def page_home():
     st.markdown(':star: 수입 지출 균형이 맞지 않는분')
     st.markdown(':fire: 여러분도 저와 ETF 투자와 함께라면 연봉 5억 가능합니다')
     st.markdown(':fire: 함께 배우고 함께 참여하세요.')
-    st.divider()
-    kakao = Image.open('kakao.jpg')
-    st.image(kakao)
-    st.write('''힘들어도 이 일을 놓지 못하는 이유 \n
-그만두기엔 좋은 분들을 너무 많이 만났고 \n
-이제 내가 없으면 안될 분들이 \n 
-여럿계시다는게 한해살이 풀 같은 영업환경에도 \n
-열심히 뛰게 만드는 이유''')
 
 def get_startend():
+    """
+    시작 날짜와 종료 날짜를 입력받아 반환함.
+
+    Returns:
+        tuple: 시작 날짜와 종료 날짜 (datetime 형식)
+    """
     col1, col2 = st.columns(2)
     start = col1.date_input('시작 날자: ', value=pd.to_datetime("2010-01-01"))
     end = col2.date_input('종료 날자: ', value=pd.to_datetime("2024-01-01"))
@@ -50,6 +52,10 @@ def get_startend():
     return (start, end)
 
 def page_graph():
+    """
+    주식 종목의 종가 그래프를 출력함.
+    종목 코드를 입력받아 해당 종목의 종가를 그래프로 표시함.
+    """
     ticker = st.text_input('주식 종목을 입력하세요', value='SPY')
     start, end = get_startend()
     data = yf.download(ticker, start=start, end=end)
@@ -70,13 +76,20 @@ def page_graph():
         st.caption('사이드바에서 입력한 시작 날짜와 종료 날짜 사이 값으로 입력해주세요')
         if start <= one_date <= end:
             if one_date in data.index:
-                st.write(data.loc[pd.to_datetime(one_date)]) #todo : 표로 바꾸기, 안됨 ㅅㅂ
+                st.write(data.loc[pd.to_datetime(one_date)])
             else:
-                st.write('휴장일 날짜이거나, 당시 없던 종목입니다.')
+                st.write('휴장일 날짜이거나, 당시 없던 종목임.')
         else:
-            st.error("데이터가 없거나 입력 정보가 잘못되었습니다. 다시 확인해주세요.")
+            st.error("데이터가 없거나 입력 정보가 잘못되었음. 다시 확인해주세요.")
 
 def add_ticker_portfolio(ticker, weight):
+    """
+    포트폴리오에 종목과 비중을 추가함.
+
+    Args:
+        ticker (str): 주식 종목 코드
+        weight (float): 주식 비중
+    """
     if ticker and weight:
         if not ticker in st.session_state['portfolio_temp'].keys():
             st.session_state['portfolio_temp'][ticker] = weight
@@ -84,13 +97,23 @@ def add_ticker_portfolio(ticker, weight):
             st.session_state['portfolio_temp'][ticker] += weight
 
 def remove_ticker_portfolio(ticker):
+    """
+    포트폴리오에서 종목을 제거함.
+
+    Args:
+        ticker (str): 제거할 주식 종목 코드
+    """
     if ticker in st.session_state['portfolio_temp']:
         del st.session_state['portfolio_temp'][ticker]
-        st.success(f"종목 '{ticker}'이(가) 제거되었습니다.")
+        st.success(f"종목 '{ticker}'이(가) 제거되었음.")
     else:
         st.error("종목을 선택해주세요.")
 
 def page_portfolio():
+    """
+    포트폴리오 페이지를 렌더링함.
+    포트폴리오 이름을 입력받고 종목과 비중을 추가하거나 제거할 수 있음.
+    """
     with st.form("portfolio_form"):
         if not 'portfolio_temp' in st.session_state:
             st.session_state['portfolio_temp'] = {}
@@ -104,10 +127,9 @@ def page_portfolio():
         weight = st.number_input("종목 비중을 입력하세요.", min_value=0.0, max_value=100.0, step=0.1)
 
         # 종목 추가 버튼
-        st.form_submit_button("종목 추가하기", on_click=add_ticker_portfolio(ticker, weight))
+        st.form_submit_button("종목 추가하기", on_click=add_ticker_portfolio, args=(ticker, weight))
 
-
-        #추가된 항목 보여주기
+        # 추가된 항목 보여주기
         if temp:
             st.write(temp)
             selected_tickers = st.multiselect('제거할 종목을 선택하세요', list(temp.keys()))
@@ -123,38 +145,52 @@ def page_portfolio():
                 st.error("이미 존재하는 포트폴리오 이름입니다. 다른 이름을 사용해주세요.")
             else:
                 st.session_state['portfolio'][name] = temp
-                st.success(f"'{name}' 포트폴리오가 성공적으로 추가되었습니다.")
+                st.success(f"'{name}' 포트폴리오가 성공적으로 추가되었음.")
                 st.session_state['portfolio_temp'] = {}
 
-
 def get_portfolio_weighted_price(portfolio, start, end):
-    # Initialize an empty DataFrame to store the weighted prices
+    """
+    포트폴리오의 종목과 가중치를 받아 날짜별 종가*가중치의 총합을 반환함.
+
+    Args:
+        portfolio (dict): 종목과 가중치로 이루어진 포트폴리오
+        start (datetime): 시작 날짜
+        end (datetime): 종료 날짜
+
+    Returns:
+        DataFrame: 날짜별 종가*가중치의 총합 데이터프레임
+    """
+    # 가중치 가격을 저장할 빈 데이터프레임 초기화
     portfolio_data = pd.DataFrame()
 
     weight_total = sum(portfolio.values())
-    # Iterate through the portfolio dictionary
+    # 포트폴리오 사전을 순회함
     for ticker, weight in portfolio.items():
-        # Download the stock data for the given ticker
+        # 주어진 티커에 대한 주식 데이터를 다운로드함
         data = yf.download(ticker, start=start, end=end)
 
         if not data.empty:
-            # Multiply the 'Close' price by the weight
+            # 'Close' 가격에 가중치를 곱함
             data['Weighted Close'] = data['Close'] * (weight / weight_total)
 
             if portfolio_data.empty:
-                # Initialize the portfolio_data DataFrame with the first stock's data
+                # 첫 번째 주식 데이터로 portfolio_data 데이터프레임을 초기화함
                 portfolio_data = data[['Weighted Close']].copy()
             else:
-                # Add the weighted prices to the existing portfolio_data DataFrame
+                # 기존 portfolio_data 데이터프레임에 가중치 가격을 추가함
                 portfolio_data = portfolio_data.add(data[['Weighted Close']], fill_value=0)
 
-    # Rename the 'Weighted Close' column to 'Close' to match the single stock DataFrame format
+    # 'Weighted Close' 열을 'Close'로 이름을 변경하여 단일 주식 데이터프레임 형식과 일치시킴
     portfolio_data.rename(columns={'Weighted Close': 'Close'}, inplace=True)
 
     return portfolio_data
 
 def page_compare():
-    tickers = st.text_input(f'비교할 ETF 또는 주식의 종목코드를 입력하세요', 'SPY').split()
+    """
+    여러 주식의 종가와 종가 추이를 비교하는 페이지를 렌더링함.
+    사용자가 입력한 주식 종목의 종가와 상대 종가를 그래프로 표시함.
+    """
+    tickers = st.text_input('비교할 ETF 또는 주식의 종목코드를 입력하세요', 'SPY').split()
     start, end = get_startend()
 
     fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
@@ -162,94 +198,4 @@ def page_compare():
         data = yf.download(ticker, start=start, end=end)
         # 주어진 날짜 이후의 가장 가까운 거래일 찾기
         idx_after = data.index.searchsorted(start, side='left')
-        initialClose = data['Close'][idx_after]
-        data['relative_Close'] = data['Close']/initialClose
-        ax1.plot(data.index, data['Close'], label=ticker)
-        ax2.plot(data.index, data['relative_Close'], label=ticker)
-
-
-    ax1.legend()
-    ax2.legend()
-    ax1.set_title(f'{", ".join(tickers)} 종가 비교')
-    ax2.set_title(f'{", ".join(tickers)} 종가 추이 비교')
-
-    fig.tight_layout()
-    st.pyplot(fig)
-
-def page_backtest():
-    start, end = get_startend()
-    if 'portfolio' in st.session_state:
-        pt_or_stock = st.radio('선택하세요', ['포토폴리오', '단일종목'])
-    else:
-        pt_or_stock = '단일종목'
-        st.warning('포토폴리오 백테스트를 원하면 포토폴리오를 추가해주세요')
-    if 'portfolio' in st.session_state and pt_or_stock == '포토폴리오':
-        seleced_portfolio = st.selectbox('포토폴리오를 선택하세요', list(st.session_state['portfolio'].keys()))
-        data = get_portfolio_weighted_price(st.session_state['portfolio'][seleced_portfolio], start, end)
-
-    elif pt_or_stock == '단일종목':
-        ticker = st.text_input('주식 종목을 입력하세요', value='SPY')
-        data = yf.download(ticker, start=start, end=end)
-
-
-    investment_select = st.radio('투자 방법을 선택하세요', ('일회성 투자', '적금형 투자'))
-    st.divider()
-
-    col1, col2 = st.columns(2, gap='medium')
-
-    if investment_select == '일회성 투자':
-        start_money = col1.number_input('투자할 금액을 입력하세요', value=1000, step=5, placeholder='만원 단위로 입력하세요')
-        if start_money:
-            end_money = round(start_money * data['Close'].iloc[-1] / data['Close'].iloc[0])
-            col2.markdown(f"## 최종 금액: {end_money} 원")
-
-    elif investment_select == '적금형 투자':
-        month_money = col1.number_input('매월 투자할 금액을 입력하세요', value=100, step=10, placeholder='만원 단위로 입력하세요')
-        if month_money:
-            # 월별 투자 금액 계산
-            monthly_investments = pd.DataFrame(index=pd.date_range(start=start, end=end, freq='MS')) # MS is month start frequency
-            monthly_investments['Investment'] = month_money
-            monthly_investments['Total Investment'] = monthly_investments['Investment'].cumsum()
-
-            # 월별 총 주식수 계산
-            monthly_investments['Close'] = data['Close'].reindex(monthly_investments.index, method='ffill')  # Fill forward missing values
-            monthly_investments['Units'] = monthly_investments['Investment'] / monthly_investments['Close']
-            monthly_investments['Total Units'] = monthly_investments['Units'].cumsum()
-
-            #월별 총 금액, 수익률 계산
-            monthly_investments['Total Value'] = monthly_investments['Total Units'] * monthly_investments['Close']
-            monthly_investments['Total ROI'] = (monthly_investments['Total Value'] / monthly_investments['Total Investment'] - 1) * 100
-
-            final_value = round(monthly_investments['Total Value'].iloc[-1])
-            final_investment = round(monthly_investments['Total Investment'].iloc[-1])
-            final_ROI = monthly_investments['Total ROI'].iloc[-1]
-
-            col2.write(f"투자 기간 : {len(monthly_investments)}월")
-            col2.write(f"최종 금액 : {final_value} 만원")
-            col2.write(f"총 투자 금액 : {final_investment} 만원")
-            col2.write(f"수익률 : {final_ROI:.2f}%")
-
-            #수익 그래프
-            fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
-            ax1.plot(monthly_investments['Total ROI'], label='ROI')
-            ax2.plot(monthly_investments['Total Value'], label='Value')
-            ax2.plot(monthly_investments['Total Investment'], label='Investment')
-            ax2.legend()
-            st.pyplot(fig)
-
-#사이드바 구성
-with st.sidebar:
-    st.title('ETF 투자 도우미')
-    page = st.radio('페이지', ['홈', '주가 그래프','비교', '포토폴리오' ,'백테스트'], index=None)
-    st.divider()
-
-if page == '홈':
-    page_home()
-elif page == '주가 그래프':
-    page_graph()
-elif page == '포토폴리오':
-    page_portfolio()
-elif page == '비교':
-    page_compare()
-elif page == '백테스트':
-    page_backtest()
+        initialClose
