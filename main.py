0df16438f39ef2bab6c3770d1ce229fe68a4a17f
htg1616 <htg1616@gmail.com>
2024-06-08 16:28:08 +0900

    함수 형태로 변경 임시

--- main.py
+++ main.py
@@ -29,7 +29,13 @@ def main_information():
     st.session_state['finalClose'] = data['Close'].asof(end)
     return (ticker, start, end, data)
 
-def graph(ticker, start, end, data):
+def graph():
+    ticker = st.text_input('주식 종목을 입력하세요', value='SPY')
+    st.write('기간을 입력하세요')
+    start = st.date_input('시작 날자: ', value=pd.to_datetime("2010-01-01"))
+    end = st.date_input('종료 날자: ', value=pd.to_datetime("2024-01-01"))
+    data = yf.download(ticker, start=start, end=end)
+
     st_graph_col1, st_graph_col2 = st.columns([7, 3])
     with st_graph_col1:
         fig1, ax1 = plt.subplots()  # 새로운 figure 생성
@@ -41,25 +47,61 @@ def graph(ticker, start, end, data):
         st.pyplot(fig1)
 
     with st_graph_col2:
-        one_date = pd.to_datetime(
-            st.date_input(label='언제 주가를 알려줄까?', value=start, min_value=start, max_value=end))
+        one_date = st.date_input(label='언제 주가를 알려줄까?', value=start, min_value=start, max_value=end)
         st.caption('사이드바에서 입력한 시작 날짜와 종료 날짜 사이 값으로 입력해주세요')
         if start <= one_date <= end:
             if one_date in data.index:
-                st.write(data.loc[one_date])
+                st.write(data.loc[pd.to_datetime(one_date)]) #todo : 표로 바꾸기, 안됨 ㅅㅂ
             else:
                 st.write('휴장일 날짜이거나, 당시 없던 종목입니다.')
         else:
             st.error("데이터가 없거나 입력 정보가 잘못되었습니다. 다시 확인해주세요.")
 
-def compare(ticker, start, end, data):
-    compare_tickers = st.text_input(f'{ticker}와 비교할 ETF 또는 주식의 종목코드를 입력하세요', 'QQQ' if ticker == 'SPY' else 'SPY').split()
-    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, )
-    ax1.plot(data.index, data['Close'], label=ticker)
+def add_ticker_portfolio(ticker):
+    if not ticker in st.session_state['portfolio']
+def portfolio(): #todo:테스트 하기
+    with st.form("portfolio_form"):
+        st.session_state['portfolio'] = {}
+        # 포트폴리오 이름 입력
+        name = st.text_input("포트폴리오 이름을 입력하세요.", key="portfolio_name")
 
-    data['relative_Close'] = data['Close']/st.session_state['initialClose']
-    ax2.plot(data.index, data['relative_Close'], label=ticker)
+        # 종목과 비중 입력
+        ticker = st.text_input("추가할 종목 이름을 입력하세요.", key="stock_name")
+        weight = st.number_input("종목 비중을 입력하세요. (단위: %)", min_value=0.0, max_value=100.0, step=0.1, key="stock_weight")
 
+        # 종목 추가 버튼
+        add_button = st.form_submit_button("종목 추가하기", on_click=)
+
+        # 종목이 추가될 때 session_state에 저장하고 multiselect에 표시
+        if add_button:
+            if not ticker in portfolio.keys():
+                st.session_state['portfolio']['ticker'] = weight
+            else:
+                portfolio[ticker] += weight
+
+        # multiselect로 추가된 종목들을 보여주기
+        st.write(portfolio) #todo
+
+        # 포트폴리오 제출
+        submit_button = st.form_submit_button("포트폴리오 제출")
+        if submit_button:
+            if name in st.session_state['portfolio']:
+                st.error("이미 존재하는 포트폴리오 이름입니다. 다른 이름을 사용해주세요.")
+            else:
+                st.session_state['portfolio'][name] = portfolio
+                st.success(f"'{name}' 포트폴리오가 성공적으로 추가되었습니다.")
+
+def compare(): #todo:포토폴리오 기능 미완
+    if 'portfoilo' in st.session_state:
+        selected_portfolio = st.multiselect(st.session_state['portfolio'].keys())
+    else:
+        selected_portfolio = []
+    start = st.session_state['start'] if 'start' in st.session_state else pd.to_datetime("2010-01-01")
+    end = st.session_state['end'] if 'end' in st.session_state else pd.to_datetime("2024-01-01")
+    selected_tickers = st.text_input(f'비교할 ETF 또는 주식의 종목코드를 입력하세요', 'QQQ' if ticker == 'SPY' else 'SPY').split()
+    compare_tickers = selected_portfolio + selected_tickers
+
+    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
     for compare_ticker in compare_tickers:
         compare_data = yf.download(compare_ticker, start=start, end=end)
         # 주어진 날짜 이후의 가장 가까운 거래일 찾기
@@ -71,8 +113,8 @@ def compare(ticker, start, end, data):
 
     ax1.legend()
     ax2.legend()
-    ax1.set_title(f'{ticker} vs {", ".join(compare_tickers)} 종가 비교')
-    ax2.set_title(f'{ticker} vs {", ".join(compare_tickers)} 종가 추이 비교')
+    ax1.set_title(f'{", ".join(compare_tickers)} 종가 비교')
+    ax2.set_title(f'{", ".join(compare_tickers)} 종가 추이 비교')
 
     fig.tight_layout()
     st.pyplot(fig)
@@ -127,14 +169,16 @@ def backtest(ticker, start, end, data):
 #사이드바 구성
 with st.sidebar:
     st.title('ETF 투자 도우미')
-    st.radio('페이지', ['주가 그래프', '비교', '백테스트'], index=None, key='page')
+    st.radio('페이지', ['주가 그래프', '포토폴리오','비교' ,'백테스트'], index=None, key='page')
     st.divider()
     ticker, start, end, data = main_information()
 
 page = st.session_state['page']
 if page == '주가 그래프':
-    graph(ticker, start, end, data)
+    graph()
+elif page == '포토폴리오':
+    portfolio()
 elif page == '비교':
-    compare(ticker, start, end, data)
+    compare()
 elif page == '백테스트':
     backtest(ticker, start, end, data)
